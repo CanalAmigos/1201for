1e26e19b215841e5c0c27d35006bc94b4a376d7e
CanalAmigos <100794225+CanalAmigos@users.noreply.github.com>
2025-09-13 12:48:44 -0300

    Update AncestorUiLibrary.lua

--- AncestorUiLibrary.lua
+++ AncestorUiLibrary.lua
@@ -45,59 +45,61 @@ lib.SaveFunctions.TemplateTable = function(type,value)
 end
 
 function lib.SaveFunctions:TransformInJson(v: 'Primitive'): {("type" & string) | ("value" & {any}) | ("version" & string)}
-	if typeof(v) == 'CFrame' then
-		return lib.SaveFunctions.TemplateTable('CFrame',{v:GetComponents()})
-	elseif typeof(v) == 'Vector3' then
-		return lib.SaveFunctions.TemplateTable('Vector3',{v.X,v.Y,v.Z})
-	elseif typeof(v) == 'Vector2' then
-		return lib.SaveFunctions.TemplateTable('Vector2',{v.X,v.Y})
-	elseif typeof(v) == 'UDim' then
-		return lib.SaveFunctions.TemplateTable('UDim',{v.Scale,v.Offset})
-	elseif typeof(v) == 'UDim2' then
-		return lib.SaveFunctions.TemplateTable('UDim2',{v.X.Scale,v.X.Offset,v.Y.Scale,v.Y.Offset})
-	elseif typeof(v) == 'Color3' then
-		return lib.SaveFunctions.TemplateTable('Color3',{v.R,v.G,v.B})
-	elseif typeof(v) == 'BrickColor' then
-		return lib.SaveFunctions.TemplateTable('BrickColor',{v.Number})
-	elseif typeof(v) == 'EnumItem' then
-		return lib.SaveFunctions.TemplateTable('EnumItem',{tostring(v.EnumType),v.Name})
-	elseif typeof(v) == 'Ray' then
-		return lib.SaveFunctions.TemplateTable('Ray',{{v.Origin.X,v.Origin.Y,v.Origin.Z},{v.Direction.X,v.Direction.Y,v.Direction.Z}})
-	elseif typeof(v) == 'NumberSequence' then
-		local keypoints = {}
-		for _, kp in ipairs(v.Keypoints) do
-			table.insert(keypoints, {kp.Time, kp.Value, kp.Envelope})
-		end
-		return lib.SaveFunctions.TemplateTable('NumberSequence', keypoints)
-	elseif typeof(v) == 'ColorSequence' then
-		local keypoints = {}
-		for _, kp in ipairs(v.Keypoints) do
-			table.insert(keypoints, {kp.Time, {kp.Value.R, kp.Value.G, kp.Value.B}})
-		end
-		return lib.SaveFunctions.TemplateTable('ColorSequence', keypoints)
-	elseif typeof(v) == 'Rect' then
-		return lib.SaveFunctions.TemplateTable('Rect',{v.Min.X,v.Min.Y,v.Max.X,v.Max.Y})
-	elseif typeof(v) == 'Region3' then
-		local min, max = v.CFrame.Position - v.Size/2, v.CFrame.Position + v.Size/2
-		return lib.SaveFunctions.TemplateTable('Region3',{{min.X,min.Y,min.Z},{max.X,max.Y,max.Z}})
-	elseif typeof(v) == 'NumberRange' then
-		return lib.SaveFunctions.TemplateTable('NumberRange',{v.Min,v.Max})
-	elseif typeof(v) == 'DateTime' then
-		return lib.SaveFunctions.TemplateTable('DateTime',v.UnixTimestamp)
-	elseif typeof(v) == 'Faces' then
-		local faces = {}
-		for _,e in pairs(Enum.NormalId:GetEnumItems()) do
-			if v[e.Name] then
-				table.insert(faces, e.Name)
+	if typeof(v) ~= 'table' then
+		if typeof(v) == 'CFrame' then
+			return lib.SaveFunctions.TemplateTable('CFrame',{v:GetComponents()})
+		elseif typeof(v) == 'Vector3' then
+			return lib.SaveFunctions.TemplateTable('Vector3',{v.X,v.Y,v.Z})
+		elseif typeof(v) == 'Vector2' then
+			return lib.SaveFunctions.TemplateTable('Vector2',{v.X,v.Y})
+		elseif typeof(v) == 'UDim' then
+			return lib.SaveFunctions.TemplateTable('UDim',{v.Scale,v.Offset})
+		elseif typeof(v) == 'UDim2' then
+			return lib.SaveFunctions.TemplateTable('UDim2',{v.X.Scale,v.X.Offset,v.Y.Scale,v.Y.Offset})
+		elseif typeof(v) == 'Color3' then
+			return lib.SaveFunctions.TemplateTable('Color3',{v.R,v.G,v.B})
+		elseif typeof(v) == 'BrickColor' then
+			return lib.SaveFunctions.TemplateTable('BrickColor',{v.Number})
+		elseif typeof(v) == 'EnumItem' then
+			return lib.SaveFunctions.TemplateTable('EnumItem',{tostring(v.EnumType),v.Name})
+		elseif typeof(v) == 'Ray' then
+			return lib.SaveFunctions.TemplateTable('Ray',{{v.Origin.X,v.Origin.Y,v.Origin.Z},{v.Direction.X,v.Direction.Y,v.Direction.Z}})
+		elseif typeof(v) == 'NumberSequence' then
+			local keypoints = {}
+			for _, kp in ipairs(v.Keypoints) do
+				table.insert(keypoints, {kp.Time, kp.Value, kp.Envelope})
 			end
-		end
-		return lib.SaveFunctions.TemplateTable('Faces',faces)
-	elseif typeof(v) == 'PhysicalProperties' then
-		return lib.SaveFunctions.TemplateTable('PhysicalProperties',{v.Density,v.Friction,v.Elasticity,v.FrictionWeight,v.ElasticityWeight})
-	elseif typeof(v) == 'number' then
-		local string = tostring(v)
-		if string:find('inf',1,true) or string == 'nan' then
-			return lib.SaveFunctions.TemplateTable('number',string)
+			return lib.SaveFunctions.TemplateTable('NumberSequence', keypoints)
+		elseif typeof(v) == 'ColorSequence' then
+			local keypoints = {}
+			for _, kp in ipairs(v.Keypoints) do
+				table.insert(keypoints, {kp.Time, {kp.Value.R, kp.Value.G, kp.Value.B}})
+			end
+			return lib.SaveFunctions.TemplateTable('ColorSequence', keypoints)
+		elseif typeof(v) == 'Rect' then
+			return lib.SaveFunctions.TemplateTable('Rect',{v.Min.X,v.Min.Y,v.Max.X,v.Max.Y})
+		elseif typeof(v) == 'Region3' then
+			local min, max = v.CFrame.Position - v.Size/2, v.CFrame.Position + v.Size/2
+			return lib.SaveFunctions.TemplateTable('Region3',{{min.X,min.Y,min.Z},{max.X,max.Y,max.Z}})
+		elseif typeof(v) == 'NumberRange' then
+			return lib.SaveFunctions.TemplateTable('NumberRange',{v.Min,v.Max})
+		elseif typeof(v) == 'DateTime' then
+			return lib.SaveFunctions.TemplateTable('DateTime',v.UnixTimestamp)
+		elseif typeof(v) == 'Faces' then
+			local faces = {}
+			for _,e in pairs(Enum.NormalId:GetEnumItems()) do
+				if v[e.Name] then
+					table.insert(faces, e.Name)
+				end
+			end
+			return lib.SaveFunctions.TemplateTable('Faces',faces)
+		elseif typeof(v) == 'PhysicalProperties' then
+			return lib.SaveFunctions.TemplateTable('PhysicalProperties',{v.Density,v.Friction,v.Elasticity,v.FrictionWeight,v.ElasticityWeight})
+		elseif typeof(v) == 'number' then
+			local string = tostring(v)
+			if string:find('inf',1,true) or string == 'nan' then
+				return lib.SaveFunctions.TemplateTable('number',string)
+			end
 		end
 	elseif typeof(v) == 'table' and (not v.version or v.version ~= lib.SaveFunctions.Version) then
 		local t = {}
@@ -110,7 +112,7 @@ function lib.SaveFunctions:TransformInJson(v: 'Primitive'): {("type" & string) |
 end
 
 function lib.SaveFunctions:UnTransformJson(v: {("type" & string) | ("value" & {any}) | ("version" & string)})
-	if type(v) == 'table' and v.type ~= nil and (v.version ~= nil and v.version == lib.SaveFunctions.Version) then
+	if typeof(v) == 'table' and v.type ~= nil and (v.version ~= nil and v.version == lib.SaveFunctions.Version) then
 		if v.type == 'CFrame' then
 			return CFrame.new(unpack(v.value))
 		elseif v.type == 'Vector3' then
